14b6ec16e0a9b94395f7b4b5c4d34f2e4ec44868
Akash Naik <veisthebest3@gmail.com>
2024-01-28 13:50:51 +0530

    Added visual part of products in the store

--- src/storepage/Storepage.jsx
+++ src/storepage/Storepage.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 export default function Storepage() {
+  const [products, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setProduct(json);
+      });
+  }, []);
+
   return (
     <>
-      <h1>Products</h1>
+        <ul className="productWrapper">
+          {products.map((image) => (
+            <li key={image.id}>
+              <img className="cards" src={image.image} alt="" />
+            </li>
+          ))}
+        </ul>
     </>
   );
 }
